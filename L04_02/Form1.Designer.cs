54a7030e81791dab7074e224c6c90cec626387e8
Ian Smith <imsmith1995@gmail.com>
2023-06-28 15:13:56 -0400

    Add project files.

---
+++ L04_02/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace L04_02
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnRed = new System.Windows.Forms.Button();
+            this.btnBlue = new System.Windows.Forms.Button();
+            this.btnGreen = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnRed
+            // 
+            this.btnRed.Location = new System.Drawing.Point(314, 40);
+            this.btnRed.Name = "btnRed";
+            this.btnRed.Size = new System.Drawing.Size(173, 92);
+            this.btnRed.TabIndex = 0;
+            this.btnRed.Text = "Red";
+            this.btnRed.UseVisualStyleBackColor = true;
+            this.btnRed.Click += new System.EventHandler(this.btnRed_Click);
+            // 
+            // btnBlue
+            // 
+            this.btnBlue.Location = new System.Drawing.Point(314, 179);
+            this.btnBlue.Name = "btnBlue";
+            this.btnBlue.Size = new System.Drawing.Size(173, 92);
+            this.btnBlue.TabIndex = 1;
+            this.btnBlue.Text = "Blue";
+            this.btnBlue.UseVisualStyleBackColor = true;
+            this.btnBlue.Click += new System.EventHandler(this.btnBlue_Click);
+            // 
+            // btnGreen
+            // 
+            this.btnGreen.Location = new System.Drawing.Point(314, 318);
+            this.btnGreen.Name = "btnGreen";
+            this.btnGreen.Size = new System.Drawing.Size(173, 92);
+            this.btnGreen.TabIndex = 2;
+            this.btnGreen.Text = "Green";
+            this.btnGreen.UseVisualStyleBackColor = true;
+            this.btnGreen.Click += new System.EventHandler(this.btnGreen_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 483);
+            this.Controls.Add(this.btnGreen);
+            this.Controls.Add(this.btnBlue);
+            this.Controls.Add(this.btnRed);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnRed;
+        private System.Windows.Forms.Button btnBlue;
+        private System.Windows.Forms.Button btnGreen;
+    }
+}
+
---
+++ L04_02/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace L04_02
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public void ChangeColor(Color newColor)
+        {
+            btnRed.BackColor = newColor;
+            btnBlue.BackColor = newColor;
+            btnGreen.BackColor = newColor;
+        }
+
+        private void btnRed_Click(object sender, EventArgs e)
+        {
+            ChangeColor(Color.Red);
+        }
+
+        private void btnBlue_Click(object sender, EventArgs e)
+        {
+            ChangeColor(Color.Blue);
+        }
+
+        private void btnGreen_Click(object sender, EventArgs e)
+        {
+            ChangeColor(Color.Green);
+        }
+    }
+}
+
